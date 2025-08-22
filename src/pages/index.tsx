4d785ab4d833c21ec5282ec378a4b73e8143052c
Patrick Parker <patrick.parker@empowerid.com>
2025-08-22 09:21:32 -0400

    Home: fix Marketing index link and update hero tagline

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home(): ReactNode {
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
           <div className="home-cta">
-            <Link className="button button--primary button--lg" to="/docs/marketing/index">
+            <Link className="button button--primary button--lg" to="/docs/marketing">
               Marketing Overview
             </Link>
             <Link className="button button--secondary button--lg" to="/docs/marketing/positioning">
