e8cd32a7bb133f3d5628afb6aeee47aa7cbabeab
patrickparker <patrickparker@users.noreply.github.com>
2025-09-20 18:33:54 -0400

    ci:rm-source_content-before-build-and-exclude

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -49,7 +49,7 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          exclude: ['**/source_content/**'],
+          exclude: ['**/source_content/**', 'source_content/**', '**/docs/source_content/**'],
         },
         blog: false,
         theme: {
